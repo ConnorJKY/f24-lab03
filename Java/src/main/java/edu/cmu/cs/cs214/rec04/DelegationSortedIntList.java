601bc6846a4e5b50e2c14608def67edec370b7a9
ConnorJKY <keyoujiang2023@gmail.com>
2024-09-12 16:39:03 -0400

    Write Class InheritanceSortedIntList to implements IntegerList for add and addAll, use totalAdded to number the times of adding.

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,49 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+public class DelegationSortedIntList implements IntegerList{
 
+    private int totalAdded;
+    private SortedIntList sortedIntList;
+
+    DelegationSortedIntList(){
+        sortedIntList = new SortedIntList();
+        totalAdded = 0;
+    }
+
+    @Override
+    public boolean add(int num) {
+        this.totalAdded++;
+        return sortedIntList.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        this.totalAdded += list.size();
+        return sortedIntList.addAll(list);
+    }
+
+    public int getTotalAdded() {
+        return this.totalAdded;
+    }
+
+    @Override
+    public int size() {
+        return sortedIntList.size();
+    }
+
+    @Override
+    public int get(int index) {
+        return sortedIntList.get(index);
+    }
+
+    @Override
+    public boolean remove(int num) {
+        return sortedIntList.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return sortedIntList.removeAll(list);
+    }
 }
