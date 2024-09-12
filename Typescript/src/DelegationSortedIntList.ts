2608fec0eb88922cc7c80ccd2648d588ae5e1d8c
ConnorJKY <keyoujiang2023@gmail.com>
2024-09-12 16:55:36 -0400

    write DelegationSortedIntList.ts to delegate the SortedIntList for add, addall,get, remove and so on and use TotalAdded for times of adding.

--- Typescript/src/DelegationSortedIntList.ts
+++ Typescript/src/DelegationSortedIntList.ts
@@ -7,9 +7,44 @@
  * @author Nora Shoemaker
  *
  */
+import {SortedIntList} from "./hidden/SortedIntListLibrary";
+import {IntegerList} from "./IntegerList";
 
 class DelegationSortedIntList {
   // Write your implementation below with API documentatioin
+    private totalAdded: number = 0
+    private sortedIntList: SortedIntList = new SortedIntList()
+    constructor () {
+    }
+    add(num: number): boolean {
+        this.totalAdded++
+        return this.sortedIntList.add(num)
+    }
+
+    addAll(list: IntegerList): boolean {
+        this.totalAdded += list.size()
+        return this.sortedIntList.addAll(list)
+    }
+
+    get(index: number): number {
+        return this.sortedIntList.get(index);
+    }
+
+    remove(num: number): boolean {
+        return this.sortedIntList.remove(num);
+    }
+
+    removeAll(list: IntegerList): boolean {
+        return this.sortedIntList.removeAll(list);
+    }
+
+    size(): number {
+        return this.sortedIntList.size();
+    }
+
+    getTotalAdded(): number {
+        return this.totalAdded;
+    }
 }
 
 export { DelegationSortedIntList }
